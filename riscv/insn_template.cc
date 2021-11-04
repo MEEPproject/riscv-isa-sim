b814ba1e4950a27c22670e67d5948193ca0791e6
Borja <borja.perez1@bsc.es>
2021-11-04 17:53:30 +0100

    Corrects bug in the latency for vector instructions if the MCPU is enabled

--- riscv/insn_template.cc
+++ riscv/insn_template.cc
@@ -44,7 +44,29 @@ reg_t rv64_NAME(processor_t* p, insn_t insn, reg_t pc)
         }
         else
         {
-          p->set_vpu_latency_considering_lanes();
+          //PVL in bits
+          //ELEN in bits
+          //vl in elements
+          //
+          //Assumes full pipelining 
+
+          uint16_t lane_chunks;
+          uint16_t pvl_chunks;
+
+          if(p->VU.get_vl()<=p->VU.PVL/p->VU.ELEN) //The end of the vector might be smaller
+          {
+            lane_chunks=ceil((float)p->VU.get_vl()/p->lanes_per_vpu);
+            pvl_chunks=1;
+          }
+          else
+          {
+            lane_chunks=ceil((float)p->VU.PVL/p->VU.ELEN/p->lanes_per_vpu);
+            pvl_chunks=ceil((float)p->VU.get_vl()/(p->VU.PVL/p->VU.ELEN));
+          } 
+
+          p->VU.set_event_dependent(p->curr_write_reg, 0,
+                            p->get_current_cycle() + (p->get_curr_insn_latency()+lane_chunks-1)*pvl_chunks);
+          p->VU.set_busy_until(p->get_current_cycle() + (p->get_curr_insn_latency()+lane_chunks-1)*(pvl_chunks-1)+lane_chunks);
         }
       }
       else
@@ -60,6 +82,16 @@ reg_t rv64_NAME(processor_t* p, insn_t insn, reg_t pc)
         else
         {
           p->set_vpu_latency_considering_lanes();
+          //PVL in bits
+          //ELEN in bits
+          //vl in elements
+          //
+          //Assumes full pipelining
+
+          uint16_t chunks=ceil((float)p->VU.get_vl()/p->lanes_per_vpu);
+          p->VU.set_event_dependent(p->curr_write_reg, 0,
+                                    p->get_current_cycle() + p->get_curr_insn_latency()+chunks-1);
+          p->VU.set_busy_until(p->get_current_cycle()+chunks);
         }
       }
     }
